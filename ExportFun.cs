d2840e7a1efb1f7c6a3747a5f1b365d75a538e10
没礼貌的芬兰人 <liao520ly@gmail.com>
2024-03-19 22:24:04 +0800

    init

---
+++ ExportFun.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace PdfiumPrinterWin32Dll
+{
+    public class ExportFun
+    {
+        [DllExport]
+        public static void PrintFilePath(string filePath, string printerName)
+        {
+            var printer = new PdfPrinter(printerName);
+            printer.Print(filePath);
+        }
+
+        [DllExport]
+        public static void PrintFilePathP(string filePath, string printerName, string password)
+        {
+            var printer = new PdfPrinter(printerName);
+            printer.PrintP(filePath, password);
+        }
+
+        [DllExport]
+
+        public static void PrintStream(Stream filePath, string printerName)
+        {
+            var printer = new PdfPrinter(printerName);
+            printer.Print(filePath);
+        }
+
+        [DllExport]
+
+        public static void PrintStreamP(Stream filePath, string printerName, string password)
+        {
+            var printer = new PdfPrinter(printerName);
+            printer.PrintP(filePath, password);
+        }
+    }
+}
---
+++ Orientation.cs
@@ -0,0 +1,8 @@
+﻿namespace PdfiumPrinterWin32Dll
+{
+    public enum Orientation
+    {
+        Horizontal,
+        Vertical
+    }
+}
